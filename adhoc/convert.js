edf6c27037776114c2c15191857ff09291602747
Magnus Steine <magnus@magnuss-mbp.lan>
2021-03-17 20:35:11 +0100

    Genererer enkelt datasett

--- adhoc/convert.js
+++ adhoc/convert.js
@@ -1,6 +1,3 @@
-console.log("aaaaaaaaaaaaaaaaaa")
-
-
 const csv = require('csv-parser')
 const fs = require('fs')
 const sanityClient = require('@sanity/client')
@@ -12,33 +9,29 @@ const client = sanityClient({
 
 function appendToFile(document) {
   const docAsNewLineJson = `${JSON.stringify(document)}\n`
-  fs.appendFileSync('ready-for-import.ndjson', docAsNewLineJson, {flag: 'a+'})
+  fs.appendFileSync('ready-for-import.ndjson', docAsNewLineJson, { flag: 'a+' })
 }
 
 function moviesByTitles(titles) {
-  return client.fetch('*[_type == "movie" && title in $titles]', {titles: titles})
+  return client.fetch('*[_type == "company" && title in $titles]', { titles: titles })
 }
 
 fs.createReadStream('./data/Caphub Company.csv')
   .pipe(csv())
   .on('data', data => {
-    // Assuming movie titles are semi-colon separated
-    const titles = data.MOVIES.split(';')
-    // Fetch movies with these titles 
-    moviesByTitles(titles).then(movies => {
-      // Build a Sanity document which matches your Studio type
-      const document = {
-        _type: 'studio',
-        name: data.NAME,
-        webPage: data.WEBPAGE,
-        movies: movies.map(movie => {
-          return {
-            _ref: movie._id,
-            _type: 'reference'
-          }
-        })
-      }
-      // Append the document to a file for later import
-      appendToFile(document)  
-    })
-  })
+
+    let company = {};
+    const keys = Object.getOwnPropertyNames(data)
+    .forEach(key => {
+      company[key.trim().replace(/ /g,'')] = data[key].trim();
+    });
+
+    const document = {
+      ...company,
+      Antallansatte: +company.Antallansatte,
+      _type: 'organisasjon',
+      _id: company.OrgNumber
+    };
+    
+    appendToFile(document);
+  });
